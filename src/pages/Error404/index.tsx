ddf4de2f855598a94a99ff03882b6993f90245b5
Jeffsantana <jeffersonsantana2008@hotmail.com>
2021-09-17 15:55:40 -0300

    back function

--- src/pages/Error404/index.tsx
+++ src/pages/Error404/index.tsx
@@ -4,18 +4,18 @@ import { useHistory, useParams, useLocation } from 'react-router-dom';
 
 const Error404: React.FC = () => {
   const history = useHistory();
-  const location = useLocation();
-  const params = useParams();
+  // const location = useLocation();
+  // const params = useParams();
 
-  console.log('history', history);
-  console.log('location', location);
-  console.log('params', params);
+  // console.log('history', history);
+  // console.log('location', location);
+  // console.log('params', params);
   return (
     <Result
       status="404"
       title="404"
       subTitle="Página não encontrada"
-      extra={<Button type="primary">Back Home</Button>}
+      extra={<Button type="primary" onClick={() => history.goBack()}>Back Home</Button>}
     />
   );
 };
